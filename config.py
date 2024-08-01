69416a9137a37f4d85e0b8ff04577ead02c78a7a
Stoupy51 <stoupy51@gmail.com>
2024-08-01 11:15:33 +0200

    ✨ Can now configure the video encoder

--- config.py
+++ config.py
@@ -7,11 +7,14 @@ ROOT: str = os.path.dirname(os.path.abspath(__file__)).replace("\\", "/")
 INPUT_FOLDER: str = f"{ROOT}/input"
 OUTPUT_FOLDER: str = f"{ROOT}/output"
 IMAGES_FOLDER: str = f"{ROOT}/images"
-CONVERT_TO_JPG: bool = True
-JPG_QUALITY: int = 95	# Range: 0-100
-VIDEO_FINAL_BITRATE: int = 240000	# 240 Mbps
-EXPRESS_MODE: bool = True	# If True, all the unprocessed frames will be copied to a new folder and fed to the program.
-							# Resulting in a faster process but no real progress tracking.
+
+# Configuration
+CONVERT_TO_JPG: bool = True				# If True, the frames will be converted to jpg format.
+JPG_QUALITY: int = 95					# Range: 0-100
+VIDEO_ENCODER: str = "libx265"			# libx265, libx264, libvpx-vp9, libaom-av1, libaom-av1, libaom-av1
+VIDEO_FINAL_BITRATE: int = 180000		# 180 Mbps
+EXPRESS_MODE: bool = True				# If True, all the unprocessed frames will be copied to a new folder and fed to the program.
+										# Resulting in a faster process but no real progress tracking.
 
 # Waifu2x constants
 WAIFU2X_FOLDER: str = f"{ROOT}/waifu2x-ncnn-vulkan"
--- process.py
+++ process.py
@@ -72,78 +72,81 @@ for video in videos:
 	# Get all the frames in each folder
 	frames: list[str] = os.listdir(extracted_path)
 	not_upscaled_frames: list[str] = [frame for frame in frames if frame not in upscaled_frames]
+	if not_upscaled_frames:
 
-
-	# Try to get upscaling ratio if any
-	upscaled_ratio: int = None
-	if upscaled_frames:
-		upscaled_size: tuple[int, int] = Image.open(f"{upscaled_path}/{upscaled_frames[0]}").size
-		extracted_size: tuple[int, int] = Image.open(f"{extracted_path}/{frames[0]}").size
-		upscaled_ratio = upscaled_size[0] // extracted_size[0]
-		info(f"Detected upscaling ratio: {upscaled_ratio}")
-	else:
-		if len(sys.argv) > 1:
-			upscaled_ratio = int(sys.argv[1])
+		# Try to get upscaling ratio if any
+		upscaled_ratio: int = None
+		if upscaled_frames:
+			upscaled_size: tuple[int, int] = Image.open(f"{upscaled_path}/{upscaled_frames[0]}").size
+			extracted_size: tuple[int, int] = Image.open(f"{extracted_path}/{frames[0]}").size
+			upscaled_ratio = upscaled_size[0] // extracted_size[0]
+			info(f"Detected upscaling ratio: {upscaled_ratio}")
 		else:
-			upscaled_ratio = int(input("Enter the upscaling ratio: "))
+			if len(sys.argv) > 1:
+				upscaled_ratio = int(sys.argv[1])
+			else:
+				upscaled_ratio = int(input("Enter the upscaling ratio: "))
 
 
-	# For each frame that hasn't been upscaled yet,
-	if not EXPRESS_MODE:
-		start_time: float = time.perf_counter()
-		for i, frame in enumerate(not_upscaled_frames):
+		# For each frame that hasn't been upscaled yet,
+		if not EXPRESS_MODE:
+			start_time: float = time.perf_counter()
+			for i, frame in enumerate(not_upscaled_frames):
+				
+				# Upscale the frame
+				command: list[str] = [
+					WAIFU2X_EXECUTABLE,
+					"-i", f"{extracted_path}/{frame}",
+					"-o", f"{upscaled_path}/{frame}",
+					"-n", str(NOISE_LEVEL),
+					"-s", str(upscaled_ratio),
+				]
+				time_elapsed: float = time.perf_counter() - start_time
+				average_time: float = time_elapsed / (i + 1)
+				remaining_time: float = average_time * (len(not_upscaled_frames) - i)
+				debug(f"Upscaling frame '{frame}'... ({i + 1}/{len(not_upscaled_frames)}),\tTime elapsed: {time_elapsed:.2f}s,\tAverage time: {average_time:.2f}s,\tRemaining time: {remaining_time:.2f}s")
+				subprocess.run(command, capture_output = True)
+
+				# Convert the frame to jpg
+				if CONVERT_TO_JPG and frame.endswith(".png"):
+					Image.open(f"{upscaled_path}/{frame}").save(f"{upscaled_path}/{frame.replace('.png', '.jpg')}", quality = JPG_QUALITY)
+					os.remove(f"{upscaled_path}/{frame}")
+		else:
+			# Copy the unprocessed frames to a new folder
+			express_path: str = f"{images_path}/express"
+			shutil.rmtree(express_path, ignore_errors = True)
+			os.makedirs(express_path, exist_ok = True)
+			info(f"Copying unprocessed frames to express folder due to express mode...")
+			for i, frame in enumerate(not_upscaled_frames):
+				if i % 100 == 0:
+					debug(f"Copying frame '{frame}'... ({i + 1}/{len(not_upscaled_frames)}),\tProgress: {i / len(not_upscaled_frames) * 100:.2f}%")
+				shutil.copy(f"{extracted_path}/{frame}", express_path)
+			info(f"Finished copying unprocessed frames to express folder.")
 			
-			# Upscale the frame
+			# Upscale the frames
 			command: list[str] = [
 				WAIFU2X_EXECUTABLE,
-				"-i", f"{extracted_path}/{frame}",
-				"-o", f"{upscaled_path}/{frame}",
+				"-i", express_path,
+				"-o", upscaled_path,
 				"-n", str(NOISE_LEVEL),
 				"-s", str(upscaled_ratio),
 			]
-			time_elapsed: float = time.perf_counter() - start_time
-			average_time: float = time_elapsed / (i + 1)
-			remaining_time: float = average_time * (len(not_upscaled_frames) - i)
-			debug(f"Upscaling frame '{frame}'... ({i + 1}/{len(not_upscaled_frames)}),\tTime elapsed: {time_elapsed:.2f}s,\tAverage time: {average_time:.2f}s,\tRemaining time: {remaining_time:.2f}s")
-			subprocess.run(command, capture_output = True)
+			info(f"Upscaling frames with express mode...")
+			subprocess.run(command)
 
-			# Convert the frame to jpg
-			if CONVERT_TO_JPG and frame.endswith(".png"):
-				Image.open(f"{upscaled_path}/{frame}").save(f"{upscaled_path}/{frame.replace('.png', '.jpg')}", quality = JPG_QUALITY)
-				os.remove(f"{upscaled_path}/{frame}")
-	else:
-		# Copy the unprocessed frames to a new folder
-		express_path: str = f"{images_path}/express"
-		shutil.rmtree(express_path, ignore_errors = True)
-		os.makedirs(express_path, exist_ok = True)
-		info(f"Copying unprocessed frames to express folder due to express mode...")
-		for i, frame in enumerate(not_upscaled_frames):
-			if i % 100 == 0:
-				debug(f"Copying frame '{frame}'... ({i + 1}/{len(not_upscaled_frames)}),\tProgress: {i / len(not_upscaled_frames) * 100:.2f}%")
-			shutil.copy(f"{extracted_path}/{frame}", express_path)
-		info(f"Finished copying unprocessed frames to express folder.")
-		
-		# Upscale the frames
-		command: list[str] = [
-			WAIFU2X_EXECUTABLE,
-			"-i", express_path,
-			"-o", upscaled_path,
-			"-n", str(NOISE_LEVEL),
-			"-s", str(upscaled_ratio),
-		]
-		info(f"Upscaling frames with express mode...")
-		subprocess.run(command)
+			# Convert the frames to jpg
+			if CONVERT_TO_JPG:
+				debug(f"Converting frames to jpg...")
+				to_convert: list[str] = os.listdir(upscaled_path)
+				for i, frame in enumerate(to_convert):
+					if frame.endswith(".png"):
+						Image.open(f"{upscaled_path}/{frame}").save(f"{upscaled_path}/{frame.replace('.png', '.jpg')}", quality = JPG_QUALITY)
+						os.remove(f"{upscaled_path}/{frame}")
+						debug(f"Converted '{frame}' to jpg... ({i + 1}/{len(to_convert)}")
+			
+			# Remove the express folder
+			shutil.rmtree(express_path, ignore_errors = True)
 
-		# Convert the frames to jpg
-		if CONVERT_TO_JPG:
-			debug(f"Converting frames to jpg...")
-			to_convert: list[str] = os.listdir(upscaled_path)
-			for i, frame in enumerate(to_convert):
-				if frame.endswith(".png"):
-					Image.open(f"{upscaled_path}/{frame}").save(f"{upscaled_path}/{frame.replace('.png', '.jpg')}", quality = JPG_QUALITY)
-					os.remove(f"{upscaled_path}/{frame}")
-					debug(f"Converted '{frame}' to jpg... ({i + 1}/{len(to_convert)}")
-	
 	# Convert the frames to a video
 	input_video_for_sound: str = f"{INPUT_FOLDER}/{video}"
 	command: list[str] = [
@@ -151,12 +154,13 @@ for video in videos:
 		"-r", "60",							# Set the framerate to 60
 		"-i", f"{upscaled_path}/%07d." + ("jpg" if CONVERT_TO_JPG else "png"),	# Input frames
 		"-i", input_video_for_sound,		# Input video for sound
-		"-c:v", "libx264",					# Encode the video with x264
+		"-c:v", VIDEO_ENCODER,				# Encode the video
 		"-b:v", f"{VIDEO_FINAL_BITRATE}k",	# Set the video bitrate
 		"-c:a", "copy",						# Copy the audio without re-encoding
 		"-map", "0:v:0",					# Map the first input (frames) as video
 		"-map", "1:a:0",					# Map the second input (sound) as audio
 		"-pix_fmt", "yuv420p",				# Set the pixel format to yuv420p
+		"-preset", "slow",					# Set the encoding preset to slow (slower but better quality)
 		"-y",
 		f"{OUTPUT_FOLDER}/{video}",			# Output video
 	]
@@ -166,5 +170,8 @@ for video in videos:
 # Shutdown the computer if needed
 if SHUTDOWN:
 	info("Shutting down the computer...")
-	subprocess.run(["shutdown", "/s", "/t", "0", "/f"], capture_output = False)
+	if os.name == "nt":
+		subprocess.run(["shutdown", "/s", "/t", "0", "/f"], capture_output = False)
+	else:
+		subprocess.run(["shutdown", "now"], capture_output = False)
 
